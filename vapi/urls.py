c275802f618470841f43203916120c7883a2c946
VaibhaV <vaibhakumaryou2@gmail.com>
2021-12-04 16:17:53 +0530

    hello

--- vapi/urls.py
+++ vapi/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 def home(request):
     return HttpResponse('home page')
@@ -23,5 +26,6 @@ def home(request):
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('', home),
+    ]+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     # path('', include('home.urls')),
 ]
